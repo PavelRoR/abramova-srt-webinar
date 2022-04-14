fe4df116fa2dc3724e05d3aa4549182ed5324ccd
PavelRoR <pavelvp2182@yandex.ru>
2022-04-14 22:12:17 +0500

    Main page is typeseted

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,97 @@
 $(document).ready(function() {
+        /* Якорь */
+        $(".header-menu li a").click(function (h) {
+            h.preventDefault();
+            var f = $(this).attr("href"),
+                g = $(f).offset().top;
+            $("body,html").animate({
+                scrollTop: g
+            }, 1500)
+        });
+    $(function () {
+        var check = $('.check', this),
+            email = $('.input-mail', this),
+            message = $('.alert-message', this),
+            button = $('.button-form', this);
+        $(".form").on("submit", function () {
+            var check = $('.check', this),
+                message = $('.alert-message', this),
+                reNone = /.+/,
+                email = $('.input-mail', this),
+                button = $('.button-form', this);
+            if (!email.val().match(reNone)) {
+                message.text('Введите email').slideDown(500);
+                return false;
+            }
+            if (!check.prop("checked")) {
+                check.next().css({
+                    'color': 'red',
+                    'transition': 'all .4s ease'
+                });
+                check.next().children().css({
+                    'color': 'red',
+                    'transition': 'all .4s ease'
+                });
+                message.text('Подтвердите соглашение').slideDown(500);
+                return false;
+            }
+            if (email.val() && check) {
 
+                button.text('Отправляем...');
+                setTimeout(function () {
+                    button.text('Отправлено!');
+                }, 500);
+                return true
+            }
+        });
+        email.click(function () {
+            // email.css({"borderColor": "rgb(25, 10, 12)",'transition':'all .4s ease'});
+            message.slideUp(500);
+        });
+        check.click(function () {
+            check.next().css({
+                "color": "#000",
+                'transition': 'all .4s ease'
+            });
+            check.next().children().css({
+                "color": "#000",
+                'transition': 'all .4s ease'
+            });
+            message.slideUp(500);
+        });
+    });
+        // Модалка
+        $('.button-up').magnificPopup({
+            type: 'inline',
+            midClick: true,
+            mainClass: 'mfp-fade'
+        });
+    $('.revs-slider-text').slick({
+        autoplay: false,
+        autoplaySpeed: 3000,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+        speed: 300,
+        arrows: true,
+        centerPadding: '40px',
+        // adaptiveHeight: true,
+        centerMode: true,
+        // appendArrows: '.video-revs-arrows-2',
+        responsive: [
+        {
+            breakpoint: 481,
+            settings: {
+                slidesToShow: 1
+            }},{
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 3
+            }
+        }
+        ]
+    });
 
 
 /*Конец документа*/
